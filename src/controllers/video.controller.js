5d44ad2a52933602964c2ab9134ce64695b56b23
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-05-04 00:53:00 +0200

    add all remaining controllers to video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,4 +1,5 @@
 import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -91,6 +92,11 @@ const publishVideo = asyncHandler(async (req, res) => {
   if ([title, description].some((field) => field?.trim() === "")) {
     throw new ApiError(400, "Title and Description required");
   }
+
+  if (!req.user?._id) {
+    throw new ApiError(400, "You are not authorized to publish a video");
+  }
+
   const videoFileLocalPath = req.files?.videoFile[0]?.path;
   const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
 
@@ -104,20 +110,308 @@ const publishVideo = asyncHandler(async (req, res) => {
   console.log(videoFile);
 
   if (!videoFile && !thumbnail) {
-    throw new ApiError(400, "Video and thubnail is required");
+    throw new ApiError(
+      500,
+      "Error uploading video and thumbnail on cloudinary"
+    );
   }
 
-  const video = await Video.create({
+  const createVideo = await Video.create({
     videoFile: videoFile.url,
     thumbnail: thumbnail.url,
-    title,
-    description,
-    owner: req.user._id,
+    duration: videoFile.duration,
+    title: title.trim(),
+    description: description.trim(),
+    owner: req.user?._id,
+    isPublished: true,
+  });
+
+  if (!createVideo) {
+    throw new ApiError(400, "videoUpload failed please try again !!!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createVideo, "Video Published successfully"));
+});
+
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(
+      404,
+      "No videoId provided. Please include a valid videoId in the request parameters."
+    );
+  }
+
+  if (!isValidObjectId(req.user?._id)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+            },
+          },
+          {
+            $project: {
+              "owner.avatar.url": 1,
+              "owner.username": 1,
+              content: 1,
+              cratedAt: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: "$subscribers",
+              },
+              isSubscribed: {
+                $cond: {
+                  if: {
+                    $in: [req.user?._id, "$subscribers.subscriber"],
+                  },
+                  then: true,
+                  else: false,
+                },
+              },
+            },
+          },
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              subscriberCount: 1,
+              isSubscribed: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        likedCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        title: 1,
+        description: 1,
+        views: 1,
+        createdAt: 1,
+        duration: 1,
+        comments: "$comments",
+        owner: 1,
+        likesCount: 1,
+        isLiked: 1,
+        thumbnail: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).json(new ApiResponse(200, video, "Video Details"));
+});
+
+const updateVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(
+      404,
+      "No videoId provided. Please include a valid videoId in the request parameters."
+    );
+  }
+  const { title, description } = req.body;
+
+  if (!(title && description)) {
+    throw new ApiError(400, "title and description are required");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "No video found");
+  }
+
+  if (video.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You can't edit this video as you are not the owner"
+    );
+  }
+
+  const thumbnailLocalPath = req.file?.path;
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "thumbnail is required");
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!thumbnail) {
+    throw new ApiError(500, "Thumbnail not updated");
+  }
+
+  const updatedVideo = Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title: title.trim().toString(),
+        description: description.trim().toString(),
+        thumbnail: thumbnail.url,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedVideo) {
+    throw new ApiError(500, "Failed to update video please try again");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedVideo, "Video updated successfully"));
+});
+
+const deleteVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (video.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You can't delete this video as you are not the owner"
+    );
+  }
+
+  const deletedVideo = Video.findByIdAndDelete(videoId);
+
+  if (!deletedVideo) {
+    throw new ApiError(500, "Couldnt Delete the Video");
+  }
+
+  await Like.deleteMany({
+    video: videoId,
+  });
+
+  await Comment.deleteMany({
+    video: videoId,
   });
 
   return res
     .status(200)
-    .json(new ApiResponse(200, video, "Video Published successfully"));
+    .json(new ApiResponse(200, {}, "Video Deleted Successfully !!!"));
 });
 
-export { getAllVideos, publishVideo };
+const togglePublishStatus = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "No video found");
+  }
+
+  if (video.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You can't edit this video as you are not the owner"
+    );
+  }
+
+  const toggledVideoPublish = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: { isPublished: !video?.isPublished },
+    },
+    { new: true }
+  );
+
+  if (!toggledVideoPublish) {
+    throw new ApiError(500, "Failed to toogle video publish status");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { isPublished: toggledVideoPublish.isPublished },
+        "Video publish toggled successfully"
+      )
+    );
+});
+
+export {
+  getAllVideos,
+  publishVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
