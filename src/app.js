74a6dcb6603cddf8219a0210e822803d567298be
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-05-05 00:23:09 +0200

    added all the playlist cotrollers

--- src/app.js
+++ src/app.js
@@ -20,10 +20,12 @@ app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 
 // routes declaration
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1playlists", playlistRouter);
 
 export { app };
